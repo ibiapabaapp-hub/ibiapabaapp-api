274929ddb73038672e12aee23540f0113452df2b
Manuel C. <manuelwn21@gmail.com>
2026-01-15 18:18:30 -0300

    feat(ConfigModule): adds config module for .env retrieving and LeadsModule to app root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LeadsModule } from './leads/leads.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
-  imports: [LeadsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    LeadsModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
