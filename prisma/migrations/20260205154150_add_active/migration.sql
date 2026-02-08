d690cfd602078d2e63776e735112a072ae471ed7
Manuel C. <manuelwn21@gmail.com>
2026-02-07 23:48:45 -0300

    feat(schema.prisma): adds user, company and user_companies tables

---
+++ prisma/migrations/20260205154150_add_active/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "companies" ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT true;
