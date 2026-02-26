19f51344cb8d8014013202a1def418831c052919
Manuel C. <manuelwn21@gmail.com>
2026-02-26 20:09:00 -0300

    feat(AppModule): adds all new modules to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppService } from './app.service';
 import { LeadsModule } from './leads/leads.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './common/guards/auth.guard';
+import { JwtModule } from './common/jwt/jwt.module';
 
 @Module({
   imports: [
@@ -11,10 +16,19 @@ import { PrismaModule } from './common/prisma/prisma.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    LeadsModule,
     PrismaModule,
+    AuthModule,
+    LeadsModule,
+    UsersModule,
+    JwtModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
