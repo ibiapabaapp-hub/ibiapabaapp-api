d78b248331ef66550bc092d3a5a77e5b7aaefd39
Manuel C. <manuelwn21@gmail.com>
2026-02-26 19:55:35 -0300

    feat(PasswordModule): adds password hashing and verifying module using argon2 + unit tests

---
+++ src/common/password/password.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+
+@Injectable()
+export class PasswordService {
+  async hashPassword(password: string): Promise<string> {
+    if (!password) {
+      throw new Error('Password string is required for hashing');
+    }
+    return argon2.hash(password);
+  }
+
+  async verifyPassword(hash: string, password: string): Promise<boolean> {
+    if (!password) {
+      throw new Error('Password string is required for verifying');
+    }
+
+    if (!hash) {
+      throw new Error('Password hash is required for verifying');
+    }
+
+    return argon2.verify(hash, password);
+  }
+}
