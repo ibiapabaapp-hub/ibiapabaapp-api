1812f8a16448404a6b35acd81ee041d3dfef07a3
Manuel C. <manuelwn21@gmail.com>
2026-02-26 21:56:20 -0300

    fix: vercel serverless function export in main

--- src/main.ts
+++ src/main.ts
@@ -3,45 +3,58 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
+import { Request, Response } from 'express';
+
+let app: NestExpressApplication;
 
 export async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['error', 'warn', 'log', 'verbose'],
-  });
+  if (!app) {
+    app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      logger: ['error', 'warn'],
+    });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      forbidUnknownValues: true,
-      transform: true,
-    }),
-  );
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        forbidUnknownValues: true,
+        transform: true,
+      }),
+    );
 
-  app.setGlobalPrefix('/api');
+    app.setGlobalPrefix('/api');
 
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: '1',
+    });
 
-  app.use(cookieParser());
+    app.use(cookieParser());
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://ibiapabaapp.com.br',
-      'https://www.ibiapabaapp.com.br',
-      'https://ibiapabaapp-landingpage.vercel.app',
-    ],
-    // credentials: true,
-    // cookies no CORS
-  });
+    app.enableCors({
+      origin: [
+        'http://localhost:3001',
+        'https://ibiapabaapp.com.br',
+        'https://www.ibiapabaapp.com.br',
+        'https://ibiapabaapp-landingpage.vercel.app',
+      ],
+      // credentials: true,
+      // cookies no CORS
+    });
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
+    await app.init();
+  }
+  return app.getHttpAdapter().getInstance();
 }
 
 bootstrap().catch((err) => {
   console.error(err);
   process.exit(1);
 });
+
+export default async (req: Request, res: Response) => {
+  const instance = await bootstrap();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return instance(req, res);
+};
