43657fcc078541e435e1f3f064a71269c081f81e
Manuel C. <manuelwn21@gmail.com>
2026-03-02 12:20:05 -0300

    fix(main.ts): conditionally set up Swagger documentation based on environment

--- src/main.ts
+++ src/main.ts
@@ -51,8 +51,10 @@ export async function bootstrap() {
       .setVersion('1.0')
       .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document);
+    if (process.env.NODE_ENV !== 'production') {
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('docs', app, document);
+    }
 
     await app.listen(process.env.PORT ?? 3000);
     await app.init();
