d8a4416149aac037945e500645bf8015f1306dda
Manuel C. <manuelwn21@gmail.com>
2026-03-02 12:14:00 -0300

    docs(app.controller.ts): documents app root test route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './common/decorators/public.decorator';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller({ version: '1' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Teste Hello World' })
+  @ApiResponse({ status: 200 })
   @Get()
   @Public()
   getHello() {
