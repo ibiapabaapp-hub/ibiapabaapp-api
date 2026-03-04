66f5723e1f88279eee4ed4ad0b66ece012aede6a
Manuel C. <manuelwn21@gmail.com>
2026-03-04 12:41:33 -0300

    docs(UsersController): documents users endpoints

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,2 +1,14 @@
-import { users } from '@prisma/client';
-export type User = users;
+import { $Enums, users } from '@prisma/client';
+export class User implements users {
+  name: string;
+  id: string;
+  birth_date: Date;
+  username: string;
+  email: string;
+  phone_number: string;
+  password: string;
+  role: $Enums.user_role;
+  active: boolean;
+  created_at: Date | null;
+  updated_at: Date | null;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,32 +12,51 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { user_role } from '@prisma/client';
 import { UserRoles } from 'src/common/decorators/user-roles.decorator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { SecureUserDto } from './dtos/secure-user-dto';
 
 @Controller({ path: 'users', version: '1' })
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // Post para criar usuário não é necessário, dado que isso acontece no em /auth/register
-  // TODO:  dúvida: seria bom ter o createUser dentro de UsersService para AuthModule chamar? Ele usa prismaService direto
-
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Obtém todos os usuários' })
+  @ApiResponse({ status: 200, type: SecureUserDto, isArray: true })
   @Get()
   @UserRoles([user_role.superuser])
   findAll(@Query() paginationDto: PaginationDto) {
     return this.usersService.findAll(paginationDto);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Buscar um usuário pelo ID' })
+  @ApiParam({ name: 'id', description: 'UUID do usuário' })
+  @ApiResponse({ status: 200, type: SecureUserDto })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   @Get(':id')
   @UserRoles([user_role.superuser])
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Atualizar dados de um usuário' })
+  @ApiResponse({ status: 200, type: SecureUserDto })
+  @Patch(':id')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   // TODO: refatorar para permitir que usuário delete sua conta através da senha correta
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Remover um usuário' })
+  @ApiResponse({ status: 200, description: 'Mensagem de sucesso' })
   @Delete(':id')
   @UserRoles([user_role.superuser])
   remove(@Param('id') id: string) {
