32e2151865d73c498451f90171964afc9e5b2fe7
Manuel C. <manuelwn21@gmail.com>
2026-03-10 12:36:28 -0300

    feat(medias): implement media upload and management service with S3 integration

---
+++ src/medias/medias.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MediasService } from './medias.service';
+
+@Controller('media')
+export class MediasController {
+  constructor(private readonly mediaService: MediasService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(@UploadedFile() file: Express.Multer.File) {
+    return this.mediaService.upload(file);
+  }
+
+  @Delete(':key')
+  async delete(@Param('key') key: string) {
+    return this.mediaService.delete(key);
+  }
+}
