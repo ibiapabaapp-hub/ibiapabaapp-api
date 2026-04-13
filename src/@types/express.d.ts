b1c45189779a076e79b994a78760adb0efdf693f
Manuel C. <manuelwn21@gmail.com>
2026-04-13 11:43:28 -0300

    chore(express.d.ts): declares request interface on express module

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,10 +1,10 @@
 import 'express';
 
 declare module 'express' {
-  interface Request {
-    user?: {
-      id: string;
-      role: string;
-    };
-  }
+	interface Request {
+		user?: {
+			id: string;
+			role: string;
+		};
+	}
 }
