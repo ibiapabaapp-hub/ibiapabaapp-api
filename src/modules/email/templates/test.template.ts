02e27648cd02bcdfad94fe5ea7596e1d34062c36
Manuel C. <1manuelc.dev@gmail.com>
2026-06-28 12:44:24 -0300

    refactor(email): remove strategy pattern, use Resend directly

--- src/modules/email/templates/test.template.ts
+++ src/modules/email/templates/test.template.ts
@@ -1,6 +1,6 @@
 export function testEmailTemplate(token: string): string {
 	return `
-    <h2>Olá, do IbiapabaApp!</h2>
+    <h2>Olá, do IbiVibe!</h2>
     <p>Teste de envio de e-mail com token :D</p>
     <p><strong>${token}</strong></p>
   `;
