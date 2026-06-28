f71f2e8110175fac7153a21f5f7c12c510c03680
Manuel C. <1manuelc.dev@gmail.com>
2026-06-28 12:44:21 -0300

    refactor(password): convert PasswordService to utility functions

---
+++ src/modules/common/password/password.util.ts
@@ -0,0 +1,20 @@
+import * as argon2 from 'argon2';
+
+export async function hashPassword(password: string): Promise<string> {
+	if (!password) {
+		throw new Error('Password string is required for hashing');
+	}
+	return argon2.hash(password);
+}
+
+export async function verifyPassword(hash: string, password: string): Promise<boolean> {
+	if (!password) {
+		throw new Error('Password string is required for verifying');
+	}
+
+	if (!hash) {
+		throw new Error('Password hash is required for verifying');
+	}
+
+	return argon2.verify(hash, password);
+}
