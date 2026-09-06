50677a03e90baadff422f638dba568a658d01b65
Manuel C. <1manuelc.dev@gmail.com>
2026-09-06 17:59:47 -0300

    feat(businesses): secure profile and gallery image uploads

--- src/modules/medias/medias.module.ts
+++ src/modules/medias/medias.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { MediasController } from './medias.controller';
 import { MediasService } from './medias.service';
 
 @Module({
 	imports: [ConfigModule],
-	controllers: [MediasController],
 	providers: [MediasService],
 	exports: [MediasService],
 })
